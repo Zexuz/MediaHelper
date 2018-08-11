7d4c30a986ef36803fa89268c7cd662befd4ff54
Zexuz <robin.edbom@gmail.com>
2018-08-11 15:22:43 +0200

    Updated ui

--- MediaHelper.Blazor/MediaHelper.Blazor.App/wwwroot/js/site.js
+++ MediaHelper.Blazor/MediaHelper.Blazor.App/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 function addTooltip() {
+    init()
+    document.addEventListener('DOMContentLoaded', function() {
+       init()
+    });
+}
+
+function init() {
     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
     M.Modal.init(document.querySelectorAll('.modal'));
+    M.Collapsible.init(document.querySelectorAll('.collapsible'));
+    console.log("started");
 }
